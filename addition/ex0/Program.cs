f5dabfba7580b04030ab1585697e481c8cdf9187
Вита Ковалева <vitakovaleva@gazprombank.ru>
2022-11-15 12:08:44 +0300

    first ex

---
+++ addition/ex0/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите целое число и нажмите Enter: ");
+String userInput = Console.ReadLine();
+int number = int.Parse(userInput);
+int result = number * number;
+
+Console.WriteLine($"{number} -> {result}");
