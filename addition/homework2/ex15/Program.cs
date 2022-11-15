61fc38da0dbcd7b9a84040d8e39790f7d34cd952
Вита Ковалева <vitakovaleva@gazprombank.ru>
2022-11-15 21:45:37 +0300

    HW ex15

---
+++ addition/homework2/ex15/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите число: ");
+int dayOfWeek = int.Parse(Console.ReadLine());
+if(dayOfWeek==6 || dayOfWeek==7){
+    Console.WriteLine("Выходной!");
+} else {
+    Console.WriteLine("Рабочий день!");
+}
