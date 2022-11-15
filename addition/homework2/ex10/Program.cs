7201cf9421b4ce2547e3f93c7a4b2a64e8dd7dd9
Вита Ковалева <vitakovaleva@gazprombank.ru>
2022-11-15 21:12:39 +0300

    HW ex10

---
+++ addition/homework2/ex10/Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите трёхзначное число: ");
+String number = Console.ReadLine();
+Console.WriteLine(number.Substring(1,1));
