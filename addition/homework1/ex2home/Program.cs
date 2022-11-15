877217deed0df88e638597781f5d17ab7d0a7efd
Вита Ковалева <vitakovaleva@gazprombank.ru>
2022-11-15 13:15:22 +0300

    HW ex2

---
+++ addition/homework1/ex2home/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите первое число: ");
+ int firstNumber = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+ int secondNumber = int.Parse(Console.ReadLine());
+ if(firstNumber > secondNumber){
+    Console.WriteLine($" max = {firstNumber}; min = {secondNumber}");
+ } else{
+    Console.WriteLine($" min = {firstNumber}; max = {secondNumber}");
+ }
