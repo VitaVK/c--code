6b19c41ae8e1d9bb244c68a669a87b574670c941
Вита Ковалева <vitakovaleva@gazprombank.ru>
2022-11-15 13:33:50 +0300

    HW ex6

---
+++ addition/homework1/ex6home/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+if(number%2==0){
+    Console.WriteLine($"Число {number} является чётным");
+} else{
+    Console.WriteLine($"Число {number} не является чётным");
+}
