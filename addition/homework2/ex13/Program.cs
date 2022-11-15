5683413fedaab46cbad253236c4ba29b266cbe66
Вита Ковалева <vitakovaleva@gazprombank.ru>
2022-11-15 21:38:42 +0300

    HW ex13

---
+++ addition/homework2/ex13/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите число: ");
+String number = Console.ReadLine();
+if(number.Length < 3)
+{
+ Console.WriteLine("Третьей цифры нет!");   
+} else{
+Console.WriteLine(number.Substring(2,1));
+}
+
