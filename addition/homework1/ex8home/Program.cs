a1855ae501a4663f1b5a0381c9c4a2db2317f284
Вита Ковалева <vitakovaleva@gazprombank.ru>
2022-11-15 13:50:16 +0300

    HW ex8

---
+++ addition/homework1/ex8home/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+Console.WriteLine($"Выводим чётные числа:");
+int i = 1;
+while (i <= number)
+{
+    if(i%2==0){
+        Console.WriteLine(i);
+    }
+  i++;
+}
