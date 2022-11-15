1c02531c71d724b883422a0996d725d1aabd472a
Вита Ковалева <vitakovaleva@gazprombank.ru>
2022-11-15 12:38:55 +0300

    ex2

---
+++ addition/ex2/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine(" Введите номер дня недели: ");
+int dayOfWeek = int.Parse(Console.ReadLine());
+if(dayOfWeek==1){
+    Console.WriteLine("Понедельник");
+} else if(dayOfWeek==2){
+    Console.WriteLine("Вторник");
+} else if(dayOfWeek==3){
+    Console.WriteLine("Среда");
+} else if(dayOfWeek==4){
+    Console.WriteLine("Четверг");
+} else if(dayOfWeek==5){
+    Console.WriteLine("Пятница");
+} else if(dayOfWeek==6){
+    Console.WriteLine("Суббота");
+} else if(dayOfWeek==7){
+    Console.WriteLine("Воскресенье");
+} else{
+    Console.WriteLine("Такого дня недели нет!");
+}
