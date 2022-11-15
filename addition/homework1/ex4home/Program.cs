fa68d2780233a0189476208602b28022b724c56e
Вита Ковалева <vitakovaleva@gazprombank.ru>
2022-11-15 13:25:43 +0300

    HW ex4

---
+++ addition/homework1/ex4home/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите первое число: ");
+int firstNumber = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int secondNumber = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите третье число: ");
+int thirdNumber = int.Parse(Console.ReadLine());
+int max = 0;
+
+if(firstNumber > secondNumber && firstNumber > thirdNumber){
+    max = firstNumber;
+} else if (secondNumber > firstNumber && secondNumber > thirdNumber){
+    max = secondNumber;
+} else{
+    max = thirdNumber;
+}
+Console.WriteLine($" Максимальное число = {max}");
