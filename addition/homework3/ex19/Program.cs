e7e3fba19e01fddf37d26fc662a4a2404a1a2ebf
Вита Ковалева <vitakovaleva@gazprombank.ru>
2022-11-20 22:07:04 +0300

    HWex19

---
+++ addition/homework3/ex19/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите пятизначное число!");
+String number = Console.ReadLine();
+bool result = true;
+
+for(int first = 0, last = number.Length -1;first<last ; ++first, --last)
+{
+    if(number[first] != number[last])
+    {
+        result = false;
+    } 
+  
+}
+
+if(result==false)
+{
+   Console.WriteLine("Число не является палиндромом!");
+}
+else
+{
+    Console.WriteLine("Число является палиндромом!");
+}
+
+
+
