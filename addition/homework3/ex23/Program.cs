5769813ae42516db28b8cb930c9e5273ca1b13bc
Вита Ковалева <vitakovaleva@gazprombank.ru>
2022-11-20 22:19:42 +0300

    HWex23

---
+++ addition/homework3/ex23/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите число!");
+int number = int.Parse(Console.ReadLine());
+int i = 1;
+while (i <= number)
+{
+    Console.WriteLine(Math. Pow(i, 3));
+    i++;
+}
