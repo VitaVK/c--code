7713b357c40414719e05bf9f2bfabbedf3c6c6fe
Вита Ковалева <vitakovaleva@gazprombank.ru>
2022-11-22 23:10:22 +0300

    HWex25

---
+++ addition/homework4/ex25/Program.cs
@@ -0,0 +1,46 @@
+﻿Console.Clear();
+double A = GetNumberFromUser("Введите целое число A: ","Ошибка ввода!");
+double B = GetNumberFromUser("Введите целое число B: ","Ошибка ввода!");
+double degree = countDegree(A, B);
+Console.WriteLine($"{A} -> {degree}");
+// Выводит в консоль сообщение message,
+// преобразовывает введённую пользователем строку в число типа int.
+// В случае ошибки выводит в консоль сообщение errorMessage
+double GetNumberFromUser(string message, string errorMessage)
+{ 
+    while(true)
+    {
+        Console.Write(message);
+        bool isCorrect = double.TryParse(Console.ReadLine(), out double userNumber);
+        if(isCorrect)
+        return userNumber;
+        
+        Console.WriteLine(errorMessage);
+    }
+}
+
+double countDegree(double firstNumber, double secondNumber){
+    double result = 1;
+      if(secondNumber == 0)
+      {
+        return result;
+      }
+      else if(secondNumber >= 0)
+      {
+        for(int i = 1; i<=secondNumber; i++)
+        {
+            result*=firstNumber;
+         
+        }
+      } 
+      else 
+      {
+        secondNumber = -secondNumber;
+       for(int i = 1; i<=secondNumber; i++)
+        {
+          result*=1/firstNumber;
+         
+        }
+      }
+      return result;
+}
