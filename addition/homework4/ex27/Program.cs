8e5c54de4600d253d3c04f1290afc513a67ad412
Вита Ковалева <vitakovaleva@gazprombank.ru>
2022-11-22 23:42:34 +0300

    HWex27

---
+++ addition/homework4/ex27/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.Clear();
+int A = GetNumberFromUser("Введите целое число A: ","Ошибка ввода!");
+int func = countNumbers(A);
+Console.WriteLine($"{A} -> {func}");
+// Выводит в консоль сообщение message,
+// преобразовывает введённую пользователем строку в число типа int.
+// В случае ошибки выводит в консоль сообщение errorMessage
+int GetNumberFromUser(string message, string errorMessage)
+{ 
+    while(true)
+    {
+        Console.Write(message);
+        bool isCorrect = int.TryParse(Console.ReadLine(), out int userNumber);
+        if(isCorrect)
+        return userNumber;
+        
+        Console.WriteLine(errorMessage);
+    }
+}
+
+int countNumbers(int number){
+    int result = 0;
+    for(int i = 1; i != 0; i++){
+        result+=number % 10;
+        number/=10;
+    }
+    return result;
+}
