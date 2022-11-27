31e636ee0a99e0f8ec6c891d3d932ed89f1db30b
Вита Ковалева <vitakovaleva@gazprombank.ru>
2022-11-27 21:49:09 +0300

    HWex38

---
+++ addition/homework5/ex38/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите размер массива: ");
+int arraySize = Convert.ToInt32(Console.ReadLine());
+int[] array = GetArray(arraySize, 0, 1000);
+Console.WriteLine(String.Join(" " , array));
+Console.WriteLine("Разница между минимальным и максимальным числом массива: " + countMaxMinNumDiff(array));
+int[] GetArray(int size, int minValue, int maxValue)
+{
+   int[] res = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        res[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return res;
+}
+
+int countMaxMinNumDiff(int[] array){
+    Array.Sort(array);
+    Console.WriteLine($"min -> {array[0]}");
+    Console.WriteLine($"max -> {array[array.Length - 1]}");
+    return array[array.Length - 1] - array[0];
+}
