f3000ff44388fac2b86f11cf29fa33c605bc8c51
Вита Ковалева <vitakovaleva@gazprombank.ru>
2022-12-03 23:47:35 +0300

    HWex41

---
+++ addition/homework6/ex41/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.WriteLine("Введите размер массива: ");
+int arraySize = Convert.ToInt32(Console.ReadLine());
+int[] array = fillArray(arraySize);
+Console.WriteLine(String.Join(" " , array));
+int sum = countPositivNumbers(array);
+
+int[] fillArray(int arraySize){
+    int[] array = new int[arraySize];
+    for(int i = 0; i < arraySize; i++){
+        Console.WriteLine("Введите число: ");
+        array[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return array;
+}
+
+int countPositivNumbers(int[] array){
+    int sum = 0;
+    for(int i = 0; i < array.Length; i++){
+        if(array[i] > 0){
+            sum++;
+        }
+    }
+    Console.WriteLine("Колличество положительных чисел в массиве: " + sum);
+    return sum;
+}
+
+
