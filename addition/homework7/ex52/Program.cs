bd607b30e845b3548e30f0e4883e1ed496408979
Вита Ковалева <vitakovaleva@gazprombank.ru>
2022-12-05 23:30:01 +0300

    HWex52

---
+++ addition/homework7/ex52/Program.cs
@@ -0,0 +1,34 @@
+﻿//Задайте двумерный массив размером m×n, 
+//заполненный случайными целыми числами
+Console.Clear();
+Console.Write("Введите количество строк массива: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбцов массива: ");
+int columns = Convert.ToInt32(Console.ReadLine());
+double[] summ = GetColumnSum(rows, columns, -5 , 5); 
+PrintAverage(summ, columns);
+
+double[] GetColumnSum(int m, int n, int minValue, int maxValue)
+{
+    int[,] result = new int[m, n];
+    double[] summ = new double[n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+           result[i, j] = new Random().Next(minValue, maxValue + 1);
+           summ [i] += result[j,i];
+        }
+         Console.WriteLine();
+    }
+    Console.WriteLine(String.Join(" " , summ));
+     return summ;
+}
+
+void PrintAverage(double[] summ, int columns){
+    for (int i = 0; i< summ.Length; i++)
+            {
+                double value = summ[i]/columns;
+                Console.WriteLine($"Среднеднее арифметическое {i}-го столбца " +  value); //вывод среднего для колонки
+            }
+}
