b1fcfab3cf48775e18c31b4b07b96bdef7cc1463
Вита Ковалева <vitakovaleva@gazprombank.ru>
2022-12-11 22:16:40 +0300

    HWex60

---
+++ addition/homework8/ex60/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.Clear();
+Console.WriteLine("Задайте размер трёхмерного массива ");
+Console.WriteLine("Введите x ");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите y ");
+int y = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите z ");
+int z = Convert.ToInt32(Console.ReadLine());
+int[,,] ThreeDarray = produce3Darray(x, y, z, 10, 100);
+
+int[,,] produce3Darray(int x1, int y1, int z1, int min, int max)
+{
+    int[,,] threeDarray = new int[x, y, z];
+    for (int x = 0; x < x1; x++)
+    {
+        for (int y = 0; y < y1; y++)
+        {
+           for (int z = 0; z < z1; z++)
+           {
+             threeDarray[x, y, z] = new Random().Next(min, max);
+             Console.Write($"{threeDarray[x, y, z], 1} " + "(" + x + "," + y + "," + z + ")" + "\t");
+           }
+           Console.WriteLine();
+        }
+        Console.WriteLine();
+    }
+    return threeDarray;
+}
